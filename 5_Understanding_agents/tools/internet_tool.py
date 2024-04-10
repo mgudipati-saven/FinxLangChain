883727066a747ac21c1c998e229562e9f96298ab
Murty Gudipati <mgudipati@Murtys-MacBook-Air-2.local>
2024-04-10 19:51:55 +0530

    Added understanding agents

---
+++ 5_Understanding_agents/tools/internet_tool.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+from langchain.tools import BaseTool
+
+class InternetTool(BaseTool):
+    name: str = "internet_tool"
+    description: str = (
+        "useful when you want to read the text on any url on the internet."
+    )
+
+    def _get_text_content(self, url: str) -> str:
+        """Get the text content of a webpage with HTML tags removed"""
+        response = requests.get(url)
+        html_content = response.text
+        soup = BeautifulSoup(html_content, "html.parser")
+        for tag in ["nav", "footer", "aside", "script", "style", "img"]:
+            for match in soup.find_all(tag):
+                match.decompose()
+        text_content = soup.get_text()
+        text_content = " ".join(text_content.split())
+        return text_content
+
+    def _limit_chars(self, text: str) -> str:
+        """limit number of output characters"""
+        return text[:10_000]
+    
+    def _run(self, url: str) -> str:
+        try:
+            text_content = self._get_text_content(url)
+            return self._limit_chars(text_content)
+        except Exception as e:
+            return f"The following error occurred while trying to fetch the {url}: {e}"
+        
+    def _arun(self, url: str):
+        raise NotImplementedError("This tool does not support asynchronous execution")
+        
+if __name__ == "__main__":
+    tool = InternetTool()
+    print(
+        tool.run("https://www.saven.in")
+    )
